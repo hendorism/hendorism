0f85eabb15c9add9ae58bb324b26cc3e7bb40e2c
Aaron Dull <hendorism@gmail.com>
2019-11-03 22:03:33 -0800

    even more updates
    adjusted some styling for main index.html and adjusted some other .js things. added "today is (day)" to main index.html

--- scale selector v002/script.js
+++ scale selector v002/script.js
@@ -1,6 +1,17 @@
 console.log("working");
 var notes = ["C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Gb","A","A#/Bb","B"]
 var cButton = document.getElementById("c");
+var csButton = document.getElementById("cs");
+var dButton = document.getElementById("d");
+var dsButton = document.getElementById("ds");
+var eButton = document.getElementById("e");
+var fButton = document.getElementById("f");
+var fsButton = document.getElementById("fs");
+var gButton = document.getElementById("g");
+var gsButton = document.getElementById("gs");
+var aButton = document.getElementById("a");
+var asButton = document.getElementById("as");
+var bButton = document.getElementById("b");
 function display(scale) {
 
 }
