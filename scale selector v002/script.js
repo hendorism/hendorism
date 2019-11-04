b51e736c3976b4d6f80910a3865c3078e7c986c0
Aaron Dull <hendorism@gmail.com>
2019-11-03 21:35:16 -0800

    more updates
    added more functionallity to scale selector v002 and cleaned up the code.
    
    trying to find a way to have each scale button call the notes array starting on a different index for each scale button

---
+++ scale selector v002/script.js
@@ -0,0 +1,11 @@
+console.log("working");
+var notes = ["C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Gb","A","A#/Bb","B"]
+var cButton = document.getElementById("c");
+function display(scale) {
+
+}
+cButton.addEventListener("click", function(){
+
+    console.log("test");
+ });
+
