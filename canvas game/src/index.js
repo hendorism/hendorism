9497b2383f0c227a8a1a3fd7ec02922701bda53c
Aaron Dull <hendorism@gmail.com>
2019-11-08 22:39:09 -0800

    worked on canvas game
    tried to work on canvas game but import paddle is not working. i am going to be a little wuss and play computer games now instead of continuing to work on this problem and solve it. i dont think i can import paddle with regular javascript syntax which is wierd because thats what i thought the guy was doing in the video. this will require much further research

--- canvas game/src/index.js
+++ canvas game/src/index.js
@@ -1,5 +1,5 @@
-import Paddle from '/src/paddle';
-
+console.log("hello");
+import Paddle from 'src/paddle';
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext('2d');
@@ -10,12 +10,32 @@ const GAME_HEIGHT = 600;
 ctx.clearRect(0,0,800,600);
 
 ctx.fillStyle = '#f00';
-ctx.fillRect(20,20,20,20);
+ctx.fillRect(20,20,50,50);
 
 ctx.fillStyle = '#0f0';
-ctx.fillRect(80,80,20,20);
+ctx.fillRect(80,80,50,50);
 
+ctx.fillStyle = '#000';
+ctx.fillRect(60,60,30,30);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    let dt = timestamp - lastTime;
+    lastTime = timestamp;
+    ctx.clearRect(0, 0, 800, 600);
+    paddle.update(dt);
+    paddle.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+
+
+console.log("goodbye");
