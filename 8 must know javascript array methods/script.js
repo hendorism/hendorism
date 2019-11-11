cce0c9eeb0aaa8fa6640f6ef762595851f092437
Aaron Dull <hendorism@gmail.com>
2019-11-10 17:56:21 -0800

    sbdfsf

---
+++ 8 must know javascript array methods/script.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+const myArray = [
+    { name: Bob,      age: 52 },
+    { name: Jake,     age: 22 },
+    { name: Carol,    age: 69 },
+    { name: Jack,     age: 49 },
+    { name: Lewiston, age: 49 },
+]
+
+
+
+
+
+
+
+
+
+
+console.log("goodbye");
