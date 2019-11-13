fbfaa534d1d063449f58c4d9d577ffb297ec7ccc
Aaron Dull <hendorism@gmail.com>
2019-11-13 07:31:42 -0800

    8 must know array methods
    updated this one just a little bit and made some other small changes

--- 8 must know javascript array methods/script.js
+++ 8 must know javascript array methods/script.js
@@ -1,14 +1,18 @@
 console.log("hello");
 
 const myArray = [
-    { name: Bob,      age: 52 },
-    { name: Jake,     age: 22 },
-    { name: Carol,    age: 69 },
-    { name: Jack,     age: 49 },
-    { name: Lewiston, age: 49 },
+    { name: 'Bob',      age: 52 },
+    { name: 'Jake',     age: 22 },
+    { name: 'Carol',    age: 69 },
+    { name: 'Jack',     age: 49 },
+    { name: 'Lewiston', age: 49 },
 ]
 
+const peopleOver30 = myArray.filter((person) => {
+    return person.age >= 30
+})
 
+console.log(peopleOver30);
 
 
 
