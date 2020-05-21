23759e1eda585e933064ab12c5d182383d17456b
Aaron Dull <hendorism@gmail.com>
2020-05-20 21:52:46 -0700

    KK

---
+++ scale selector/scale_selector_v4.js
@@ -0,0 +1,19 @@
+// scale_selector_v4.js
+// 5/17/2020
+console.log("scale_selecotr_v4.js is working.")
+const pitches = ["C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B"]
+function selector(a){
+    
+    console.log(`a=${a}`)
+    console.log("my name is Aaron and the selector function has been called and the variable a  has been passed to the function. a="+a)
+    for (i=0;i<pitches.length;i++){
+        document.getElementById("").innerHTML = pitches[i+x]
+    }
+}
+
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</scrip>
+
+
