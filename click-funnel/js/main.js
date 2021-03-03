da687a094a92d4ee7dbaebf58bf8a8e2a69f7507
Aaron Dull <hendorism@gmail.com>
2021-03-02 16:01:12 -0800

    stuff

--- click-funnel/js/main.js
+++ click-funnel/js/main.js
@@ -3,4 +3,20 @@ let player = {
     y: 50,
     width: 10,
     height: 10
+    hp: 100;
 }
+let gameWorld = {
+  height: 500;
+  width: 500;
+}
+function updateRender() {
+  //redraw game screen
+  //or just update position of all the elements within div.gameWorld
+}
+function mainLoop() {
+  while(inGame == true) {
+    setInterval(updateRender(), 1000/10)
+  }
+}
+
+
